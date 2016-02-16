5f2af78225f866ce8be55f171403b32f02f71509
Etienne Dodat <etienne.dodat@gmail.com>
2016-02-17 00:41:19 +0100

    implemented custom types

---
+++ app/models/placing.rb
@@ -0,0 +1,33 @@
+class Placing
+  attr_accessor :name, :place
+
+  def initialize name, place
+    @name = name
+    @place = place
+  end
+
+  def mongoize
+    { name: @name, place: @place}
+  end
+
+  def self.mongoize object
+    case object
+    when nil then nil
+    when Hash then Placing.new(object[:name], object[:place]).mongoize
+    when Placing then object.mongoize
+    end
+  end
+
+  def self.demongoize object
+    case object
+    when nil then nil
+    when Hash then Placing.new(object[:name], object[:place])
+    when Placing then object
+    end
+  end
+
+  def self.evolve object
+    self.mongoize object
+  end
+
+end
