b73e2695859e74b3765d75cff4121e3f9757d478
Etienne Dodat <etienne.dodat@gmail.com>
2016-02-19 23:42:47 +0100

    implemented registration

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,9 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
-  resources :racers
+  resources :racers do
+    post "entries" => "racers#create_entry"
+  end
   resources :races
 
   # Example resource route with options:
